02b76aac2894f2d16fcb187ebb7a140f046e6f3f
msoli <msoli@DMITRY_LABORATY>
2024-07-24 22:12:27 -0400

    Add project files.

---
+++ Lista_de_paginas/Lista_de_paginas/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Lista_de_paginas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ir_html_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/HTML", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void Ir_css_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Hoja_de_estilos_en_cascada", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void ver_powerbi_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://powerbi.microsoft.com/es-es/what-is-power-bi/", BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
